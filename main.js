0aa619f4131434d095bed3605bab9daef92cdcbb
CODERBLOXx <78531418+CODERBLOXx@users.noreply.github.com>
2021-05-06 15:56:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function getParagraph1(){
+    var inputs = [];
+    for(var i = 1; i <= 6; i++){
+        inputs.push(document.getElementById("para1_input_box_" + i).value);
+    }
+    inputs.join(".")
+    document.getElementById("show_paragraph1").innerHTML = inputs.join(".");
+}
